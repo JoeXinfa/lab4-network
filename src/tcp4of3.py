29707b86d909f89c55089dd5d19acfb24e1d3698
Joseph Zhu <xinfazhu@gmail.com>
2019-08-11 16:54:56 -0400

    Send HTTP GET request after established TCP connection

---
+++ src/tcp4of3.py
@@ -0,0 +1,39 @@
+from scapy.all import IP, TCP, send, conf, L3RawSocket, sr1
+
+"""
+https://samsclass.info/123/proj14/p10-scapytcp.html
+
+# Drop all RST packets
+sudo iptables -F
+sudo iptables -A OUTPUT -p tcp --tcp-flags RST RST -j DROP
+sudo iptables -L
+
+sudo iptables -A OUTPUT -p tcp --tcp-flags RST RST -s <your-ip> -j DROP
+ifconfig # find your IP address
+sudo iptables -A OUTPUT -p tcp --tcp-flags RST RST -s 10.0.2.15 -j DROP
+
+# How to run and look
+sudo tcpdump -i any -w atest.pcap
+sudo python this.py
+Use Wireshark open the pcap file
+"""
+
+def main():
+    # This is required for traffic to localhost
+    conf.L3socket = L3RawSocket
+
+    my_ip = IP(dst="www.google.com")
+    my_tcp = TCP(dport=80)
+
+    pkt = my_ip / my_tcp
+    syn_ack = sr1(pkt) # client send SYN and receive SYN/ACK
+    my_tcp = TCP(dport=80, sport=syn_ack[TCP].dport, seq=syn_ack[TCP].ack,
+        ack=syn_ack[TCP].seq+1, flags='A')
+    get_str = "GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n"
+    request = my_ip / my_tcp / get_str
+    reply = sr1(request)
+
+    print type(reply)
+
+if __name__ == "__main__":
+    main()
