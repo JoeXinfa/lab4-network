55c870bd49608bcfee2489783685f0b0a51943dc
Joseph Zhu <xinfazhu@gmail.com>
2019-08-12 17:46:09 -0400

    Use socket to split GET

---
+++ src/socket_falun.py
@@ -0,0 +1,33 @@
+import socket
+
+host = "www.baiwanzhan.com"
+port = 80
+
+def main():
+    # TCP connect
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((host, port))
+    
+    # result socket_water.pcap
+    #reqs = ["GET /service/site/search.aspx?query=water " \
+    #    "HTTP/1.1\r\nHost:{}\r\n\r\n".format(host)]
+    # result socket_falun.pcap
+    #reqs = ["GET /service/site/search.aspx?query=%E6%B3%95%E8%BD%AE " \
+    #    "HTTP/1.1\r\nHost:{}\r\n\r\n".format(host)]
+    # result socket_water2.pcap
+    reqs = ["GET /service/site/search.aspx?query=wa",
+        "ter HTTP/1.1\r\nHost:{}\r\n\r\n".format(host)]
+    # result socket_falun2.pcap
+    #reqs = ["GET /service/site/search.aspx?query=%E6%B3%95",
+    #    "%E8%BD%AE HTTP/1.1\r\nHost:{}\r\n\r\n".format(host)]
+    print(reqs)
+
+    requests = [info.encode() for info in reqs]
+    responses = []
+    for req in requests:
+        client.send(req)
+        resp = client.recv(4096)
+        responses.append(repr(resp))
+
+if __name__ == "__main__":
+    main()
